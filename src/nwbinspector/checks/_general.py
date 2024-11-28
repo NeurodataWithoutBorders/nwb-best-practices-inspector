396b5517e53e65287fd25ad56b3251e1449f7f6b
Ryan Ly <rly@lbl.gov>
2024-11-27 17:30:15 -0800

    Update src/nwbinspector/checks/_general.py
    Co-authored-by: Ben Dichter <ben.dichter@gmail.com>

--- src/nwbinspector/checks/_general.py
+++ src/nwbinspector/checks/_general.py
@@ -18,7 +18,7 @@ def check_name_slashes(neurodata_object: object) -> Optional[InspectorMessage]:
 
 @register_check(importance=Importance.BEST_PRACTICE_SUGGESTION, neurodata_type=None)
 def check_name_colons(neurodata_object: object) -> Optional[InspectorMessage]:
-    """Check if an object contains a colon."""
+    """Check if an object name contains a colon."""
     if hasattr(neurodata_object, "name") and ":" in neurodata_object.name:
         return InspectorMessage(message="Object name contains colons.")
 
