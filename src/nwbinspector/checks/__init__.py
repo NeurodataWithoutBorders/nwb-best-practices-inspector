9085d835dba68eb52b9cd8b3562882278de4c657
Ryan Ly <rly@lbl.gov>
2024-11-27 17:24:48 -0800

    Fix import check_name_colons

--- src/nwbinspector/checks/__init__.py
+++ src/nwbinspector/checks/__init__.py
@@ -13,6 +13,7 @@ from ._ecephys import (
 from ._general import (
     check_description,
     check_name_slashes,
+    check_name_colons,
 )
 from ._icephys import (
     check_intracellular_electrode_cell_id_exists,
